18dc9328fafe43a301de05d5116ae051875fd8b9
amidou.zabre <amidou.zabre@gmail.com>
2025-02-25 11:26:26 +0000

    Settings: add Media_url and media_root

--- project4/settings.py
+++ project4/settings.py
@@ -121,4 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

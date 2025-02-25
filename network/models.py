3c81f01ddffc69892415353f7287c70014bd5ff2
amidou.zabre <amidou.zabre@gmail.com>
2025-02-25 11:44:37 +0000

    Network->Models: Create Profile model

--- network/models.py
+++ network/models.py
@@ -5,3 +5,10 @@ from django.db import models
 class User(AbstractUser):
     bio = models.TextField(blank=True)
     user_img = models.ImageField(upload_to='user_images/', default="blank_user_img.png")
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following')
+    website = models.URLField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)

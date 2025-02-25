1f09f18a6709e068fbb7cc38e1ab5062ccc6578c
amidou.zabre <amidou.zabre@gmail.com>
2025-02-25 11:47:13 +0000

    Network->Models: Create LikePost model

--- network/models.py
+++ network/models.py
@@ -19,3 +19,9 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class LikePost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)

9c7babeb6182092cebffbdd4093f452a73a2f3e9
amidou.zabre <amidou.zabre@gmail.com>
2025-02-25 11:48:25 +0000

    Network->Models: Create Follow model

--- network/models.py
+++ network/models.py
@@ -25,3 +25,9 @@ class LikePost(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
+    following = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)

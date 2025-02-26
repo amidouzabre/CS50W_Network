8aa0752a2b12bcc2bf045654d7896b6470edb2b3
amidou.zabre <amidou.zabre@gmail.com>
2025-02-26 15:34:50 +0000

    Network->urls: add variable on profile url

--- network/urls.py
+++ network/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("post/add/", views.post_new, name="post_add"),
+    path("profile/<str:username>", views.profile, name="profile"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register")

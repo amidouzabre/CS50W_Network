cd159bd4591cf98d06b7e317e85e7d2273bfa513
amidou.zabre <amidou.zabre@gmail.com>
2025-02-26 11:36:12 +0000

    Network->stye: add post-box and profile-img rules

--- network/urls.py
+++ network/urls.py
@@ -10,3 +10,7 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register")
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

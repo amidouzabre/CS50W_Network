8b58e5a636c505a03d1bfb8c9a1f1637226fab91
amidou.zabre <amidou.zabre@gmail.com>
2025-02-26 15:57:00 +0000

    Network->Templates: add profile.html

--- network/models.py
+++ network/models.py
@@ -8,7 +8,7 @@ class User(AbstractUser):
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     website = models.URLField(blank=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
 

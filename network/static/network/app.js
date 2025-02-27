4af60b992e459f8b538b5d038a57d413788bbd77
amidou.zabre <amidou.zabre@gmail.com>
2025-02-27 21:31:47 +0000

    Network->static: app.js change data-etable

--- network/static/network/app.js
+++ network/static/network/app.js
@@ -1,70 +1,81 @@
-posts = document.querySelectorAll('.post-box p[data-post-id]');
-const authenticatedUsername = document.querySelector('.post-box.compose').getAttribute('data-user-authenticated');
-
- // Filter the elements based on the data-username attribute
- const userPosts = Array.from(posts).filter((post) => {
-    return post.getAttribute('data-username') === authenticatedUsername;
-});
-
-userPosts.forEach(post => {
-    post.addEventListener('click', function() {
-        const postId = this.getAttribute('data-post-id');
-
-        const postForm = document.getElementById(`post-form-${postId}`);
-        postForm.hidden = !postForm.hidden;
-        post.hidden = !post.hidden;
-
-        if (!postForm.hidden) {
-            const textarea = postForm.querySelector('textarea');
-            textarea.focus();
+//posts = document.querySelectorAll('.post-box p[data-post-id]');
+posts_editables = document.querySelectorAll('.post-box p[data-post-editable]');
 
 
-            // Cancel button    
-            if(postForm.oncancel === null) {
-                postForm.oncancel = function() {
-                    postForm.hidden = true;
+// Get the authenticated user
+
+const authenticatedUser = document.querySelector('#user-authenticated');
+
+if(authenticatedUser){
+   
+    /*
+    const authenticatedUsername  = authenticatedUser.getAttribute('data-user-authenticated');
+
+    // Filter the elements based on the data-username attribute
+    const userPosts = Array.from(posts).filter((post) => {
+        return post.getAttribute('data-username') === authenticatedUsername;
+    });
+    */
+
+    posts_editables.forEach(post => {
+        post.addEventListener('click', function() {
+            const postId = this.getAttribute('data-post-id');
+
+            const postForm = document.getElementById(`post-form-${postId}`);
+            postForm.hidden = !postForm.hidden;
+            post.hidden = !post.hidden;
+
+            if (!postForm.hidden) {
+                const textarea = postForm.querySelector('textarea');
+                textarea.focus();
+
+
+                // Cancel button    
+                if(postForm.oncancel === null) {
+                    postForm.oncancel = function() {
+                        postForm.hidden = true;
+                        post.hidden = false;
+                    }
+                }
+
+                const cancelButton = postForm.querySelector('input[type="button"]');
+                cancelButton.addEventListener('click', postForm.oncancel);
+            }
+        });
+
+
+        const form = document.querySelector(`#post-form-${post.getAttribute('data-post-id')}`);
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            const content = form.querySelector('textarea').value;
+
+            try {
+                const result =  fetchJSON(`/post/edit/${post.getAttribute('data-post-id')}`, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        content: content
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-CSRFToken': form.querySelector('[name=csrfmiddlewaretoken]').value
+                    }
+                });
+                
+                if (result) {
+                    post.textContent = content;
                     post.hidden = false;
+                    form.hidden = true;
+                } else {
+                    alert('An error occurred while updating the post.');
                 }
+
+
+            } catch (error) {
+                console.log(error);
             }
-
-            const cancelButton = postForm.querySelector('input[type="button"]');
-            cancelButton.addEventListener('click', postForm.oncancel);
-        }
+        });
     });
-
-
-    const form = document.querySelector(`#post-form-${post.getAttribute('data-post-id')}`);
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        const content = form.querySelector('textarea').value;
-
-        try {
-            const result =  fetchJSON(`/post/edit/${post.getAttribute('data-post-id')}`, {
-                method: 'POST',
-                body: JSON.stringify({
-                    content: content
-                }),
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-CSRFToken': form.querySelector('[name=csrfmiddlewaretoken]').value
-                }
-            });
-            
-            if (result) {
-                post.textContent = content;
-                post.hidden = false;
-                form.hidden = true;
-            } else {
-                alert('An error occurred while updating the post.');
-            }
-
-
-        } catch (error) {
-            console.log(error);
-        }
-    });
-});
-    
+};
 
 /**
  * Fecth JSON
